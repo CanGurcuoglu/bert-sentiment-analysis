c2792cf11484eaef372446a621ce36ee7baf5994
CanGurcuoglu <gurcuoglucan1@gmail.com>
2025-02-12 10:44:58 +0300

    update

--- models/ner_model.py
+++ models/ner_model.py
@@ -1,12 +1,20 @@
 import spacy
+from config import SPACY_NER_TR_PATH, SPACY_NER_ENG_PATH
 
 class NERAnalyzer:
-    def __init__(self, model_path):
-        """Load the specified spaCy NER model."""
-        self.nlp = spacy.load(model_path)
 
-    def analyze(self, text):
+    def __init__(self):
+        # Load models
+        self.nlp_tr = spacy.load(SPACY_NER_TR_PATH)
+        self.nlp_eng = spacy.load(SPACY_NER_ENG_PATH)
+
+    def analyze(self, text, lang):
         """Perform Named Entity Recognition (NER) on the given text."""
-        doc = self.nlp(text)
-        entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
-        return entities
+        if (lang == "TR"):
+            doc = self.nlp_tr(text)
+            entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
+            return entities
+        else:
+            doc = self.nlp_eng(text)
+            entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
+            return entities      
