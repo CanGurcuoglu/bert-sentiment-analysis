0dc5c9de3fa5899de80680420a924af5530e87c1
adil-Kaygin <kayginadil@gmail.com>
2025-02-12 15:21:29 +0300

    add query

--- utils/langchain_helper.py
+++ utils/langchain_helper.py
@@ -28,6 +28,7 @@ class QueryAnalyzer:
         - 'sentiment': If the text expresses an opinion or emotion.
         - 'ner': If the text contains names, organizations, locations, dates, or numerical entities.
         - 'both': If both types of analysis are needed.
+        - 'query': If the text requires sql query like "How many negative reviews are there about Vodafone?"
         - 'else': If none of these.
 
         Input: "{text}"
@@ -62,6 +63,37 @@ class QueryAnalyzer:
             
         The reply should be natural.
 
+        """
+        try:
+            fallback_response = self.client.models.generate_content(
+                model=self.model_name,
+                contents=fallback_prompt
+            )
+        except:
+            self.reload()
+            return "an error occured. Please, try again"
+
+            # LLM'den gelen metni direkt dönüyoruz.
+        return fallback_response.text.strip()
+    
+    def query_analyzer(self,text):
+        fallback_prompt = f"""
+        You can use two function and they have required arguments:
+
+        - ope_label which requires:
+            -operator_name: must be in this list "ope_list = ["Turkcell","Vodafone","Turk Telekom","AT&T","T-mobile","ID-Mobile","O2"]"
+            -label_type: must be in this list "labels = ['OPE', 'APP', 'PAY', 'DATE', 'SER', 'OTH', 'ORG', 'NUM', 'PER', 'LOC', 'PKG', 'NET', 'LIN', 'BANK']"
+        - ope_sentiment which requires:
+            -operator_name: must be in this list "ope_list = ["Turkcell","Vodafone","Turk Telekom","AT&T","T-mobile","ID-Mobile","O2"]"
+            -sentiment_type: must be in this list "sentiment_labels = ["Negative", "Neutral", "Positive"]"
+        The user input is: "{text}"
+
+        Options:
+            [ope_label, operator_name, label_type]: If user input contains operator_name and label_type
+            [ope_sentiment, operator_name, sentiment_type]: If user input contains operator_name and sentiment_type
+            [answer]: If none of these.
+
+        Response (only one of '[ope_label, operator_name, label_type]', '[ope_sentiment, operator_name, sentiment_type]', or '[answer]'):
         """
         try:
             fallback_response = self.client.models.generate_content(
